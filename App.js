5114a6dbf4ccb5267c7d5e61689b97e4407bb8a0
Bianca S. Ramal <biancasramal@gmail.com>
2021-07-07 19:03:02 -0300

    Finish

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet, Image } from "react-native";
 
-export default function App() {
+import Constants from "expo-constants";
+import * as ImagePicker from "expo-image-picker";
+import axios from "axios";
+
+export default function Upload() {
+  const [avatar, setAvatar] = useState();
+
+  async function imagePickerCall() {
+    if (Constants.platform.ios) {
+      const { status } = await ImagePicker.requestCameraPermissionsAsync();
+
+      if (status !== "granted") {
+        alert("Nós precisamos dessa permissão.");
+        return;
+      }
+    }
+
+    const data = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All
+    });
+
+    if (data.cancelled) {
+      return;
+    }
+
+    if (!data.uri) {
+      return;
+    }
+
+    setAvatar(data);
+    console.log(data);
+  }
+
+  async function uploadImage() {
+    const data = new FormData();
+    const path = avatar.uri.split('/');
+    const name = path[path.length - 1];
+
+    data.append("path", {
+      uri: avatar.uri,
+      type: avatar.type,
+      name: name
+    });
+
+    await axios.post("http://192.168.0.93:8000/api/blobMedias", data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: avatar
+            ? avatar.uri
+            : "https://mltmpgeox6sf.i.optimole.com/w:761/h:720/q:auto/https://redbanksmilesnj.com/wp-content/uploads/2015/11/man-avatar-placeholder.png"
+        }}
+        style={styles.avatar}
+      />
+      <TouchableOpacity style={styles.button} onPress={imagePickerCall}>
+        <Text style={styles.buttonText}>Escolher imagem</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={uploadImage}>
+        <Text style={styles.buttonText}>Enviar imagem</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +71,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
+  button: {
+    width: 150,
+    height: 50,
+    borderRadius: 3,
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#fff"
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  }
 });
